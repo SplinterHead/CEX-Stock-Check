d793e3ea862775d58aba9e6c4a0cc1379cda8756
Lewis England <Lewis.England@caplin.com>
2015-03-11 10:34:52 +0000

    Initial Commit. No email notifications yet

---
+++ cex_checker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#product_code=045496510060 #3DS Circle Pad Pro
+product_code=045496510107 #3DS XL Circle Pad Pro
+
+if [[ -f product.html ]]; then
+    rm -f product.html
+fi
+
+curl -s -o product.html https://uk.webuy.com/product.php?sku=${product_code}
+
+if [[ -n $(grep "buy this item" product.html) ]]; then
+    echo "This item is in stock"
+else
+    echo "This item is still out of stock"
+fi
